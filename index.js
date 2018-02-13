71c87686df18ef34fcb4a3580f5b75b9ee0e4683
tototidan <alexis.broustaut@ynov.com>
2018-02-13 17:10:51 +0100

    base

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 const program = require('commander');
 const inquirer = require('inquirer');
-
+const discord =require('discord.js');
+const client = new discord.Client();
+client.login('NDEyOTI1OTU1ODIxMTQyMDE2.DWSa9w.dpviWpXVk0p6aDH3qidUkcgC0MU');
+client.on("ready",() =>
+{
+	console.log("salut mec ")
+})
+client.on("message",message =>{
+	if(message.content === "salut")
+	{
+		console.log("salut mec2");
+		message.reply('pong')
+	}
+})
 //Configuration des paramètres attendus
 program
     .version('1.0.0')
@@ -10,35 +23,35 @@ program
 
 //On parse les arguments
 //fonction synchrone
-program.parse(process.argv);
+// program.parse(process.argv);
 
-if(program.world){
-    console.log("Hello world");
-}
-else if(program.input){
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Entrer votre nom d\'utilisateur',
-            name: 'username'
-        },{
-            type: 'password',
-            message: 'Entrer votre mot de passe',
-            name: 'password'
-        },{
-            type: 'checkbox',
-            message: 'Que voulez-vous sauvegarder ?',
-            name: 'folderToSave',
-            choices:[
-                'Documents',
-                'Bureau',
-                'Musique'
-            ]
-        }
-    ]).then((answer)=> {
-        console.log(answer);
-    })
-}
-else{
-    program.help();
-}
+// if(program.world){
+    // console.log("Hello world");
+// }
+// else if(program.input){
+    // inquirer.prompt([
+        // {
+            // type: 'input',
+            // message: 'Entrer votre nom d\'utilisateur',
+            // name: 'username'
+        // },{
+            // type: 'password',
+            // message: 'Entrer votre mot de passe',
+            // name: 'password'
+        // },{
+            // type: 'checkbox',
+            // message: 'Que voulez-vous sauvegarder ?',
+            // name: 'folderToSave',
+            // choices:[
+                // 'Documents',
+                // 'Bureau',
+                // 'Musique'
+            // ]
+        // }
+    // ]).then((answer)=> {
+        // console.log(answer);
+    // })
+// }
+// else{
+    // program.help();
+// }
